6c6825e14c4a621b7596432f74a38b21288b5b93
Volodymyr Fouchek <vinvova555333@gmail.com>
2023-02-14 00:33:46 +0200

    Add Firebase & config

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
+
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
+
 import App from './App';
 import store from './redux/store';
+import './firebase';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
